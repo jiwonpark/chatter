7842afed56244344367d78628ecf2889478df683
salt <salt.jeong@gmail.com>
2018-08-22 13:21:42 +0900

    fix a bug for 방장모드

--- bangzang.js
+++ bangzang.js
@@ -1,6 +1,6 @@
 function Bangzang() {
     this.operation = {
-        register: '>>등록',
+        register: '>>입장',
         success: '>>완료',
         terminate: '>>종료',
         start: '>>시작'
@@ -9,15 +9,16 @@ function Bangzang() {
     this.mission = {
         isActive: false,
         successMember: [],
-        congraturations: "Congraturations!"
+        congraturations: "모두 성공하셨습니다 수고하셨습니다!"
     };
     this.mqttClient;
     this.mqttTopic;
 }
 
-Bangzang.prototype.setMqttClient = function (client, topic) {
+Bangzang.prototype.setMqttClient = function (client, topic, nickname) {
     this.mqttClient = client;
     this.mqttTopic = topic;
+    this.mqttClient.publish(this.mqttTopic, `방장(${nickname})이 입장하였습니다. 새로 입장해주세요.\n입장 + enter`);
 }
 
 Bangzang.prototype.checkSpecialOperation = function (message) {
@@ -25,14 +26,15 @@ Bangzang.prototype.checkSpecialOperation = function (message) {
 
     if (message.length - message.indexOf(this.operation.register) == 4) {
         nickname = message.slice(0, message.indexOf(this.operation.register));
-        for (var i = 0; this.members.length; i++) {
+        for (var i = 0; i < this.members.length; i++) {
             if (this.members[i] == nickname) {
                 return true;
             }
         }
     
         this.members.push(nickname);
-        this.mqttClient.publish(this.mqttTopic, `방장>>${nickname}님 등록되었습니다.`);
+        this.mqttClient.publish(this.mqttTopic, `방장>>${nickname}님 입장하셨습니다.`);
+        this.mqttClient.publish(this.mqttTopic, '=============================================');
         console.log('Current Members : ' +this.members.join(' , '));
         return true;
     }
@@ -77,6 +79,7 @@ Bangzang.prototype.successMission = function (nickname) {
 
     if (this.mission.successMember.length >= this.members.length) {
         this.mqttClient.publish(this.mqttTopic, this.mission.congraturations);
+        this.mqttClient.publish(this.mqttTopic, '=============================================');
         this.mission.successMember = [];
         this.mission.isActive = false;    
     }
@@ -86,16 +89,18 @@ Bangzang.prototype.startMission = function () {
     this.mission.successMember = [];
     this.mission.isActive = true;
     this.mqttClient.publish(this.mqttTopic, `방장>>미션이 시작되었습니다. 화이팅!`);
+    this.mqttClient.publish(this.mqttTopic, '=============================================');
 }
 
 Bangzang.prototype.terminateMission = function () {
-    this.mission.successMember = [];
-    this.mission.isActive = false;
-
     this.mqttClient.publish(this.mqttTopic, `미션을 종료합니다!`);
-    for (var i = 0; this.mission.successMember.length; i++) {
+    for (var i = 0; i < this.mission.successMember.length; i++) {
         this.mqttClient.publish(this.mqttTopic, `${this.mission.successMember[i]} 미션 성공!`);
     }
+
+    this.mqttClient.publish(this.mqttTopic, '=============================================');
+    this.mission.successMember = [];
+    this.mission.isActive = false;
 }
 
 module.exports = new Bangzang();
